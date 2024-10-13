c656c0e37542dd7bb540e24c5b2463c9288e0622
Laurence Liang <laurence.w.liang@gmail.com>
2024-10-13 02:37:11 -0400

    Add dashboard and static Cloudflare

--- dementia-insights-frontend/src/pages/upload.js
+++ dementia-insights-frontend/src/pages/upload.js
@@ -3,13 +3,58 @@ import "./globals.css";
 import axios from "axios";
 
 import { useRef, useState } from "react";
+import { useEffect } from "react";
 
 export default function Upload() {
+  const cloudflareResp = {
+    questions: [
+      { id: 1, question: "What were you doing one hour ago?" },
+      { id: 2, question: "What did you have for dinner last night?" },
+      {
+        id: 3,
+        question:
+          "Can you recall and name three objects that were in this room 10 minutes ago?",
+      },
+      {
+        id: 4,
+        question:
+          "Can you recall and describe the plot of a book you finished recently?",
+      },
+      {
+        id: 5,
+        question: "What is today’s date? (Including day, month, and year)",
+      },
+      {
+        id: 6,
+        question:
+          "Can you remember the names of three new individuals you have recently met?",
+      },
+      {
+        id: 7,
+        question: "What was the color of the shirt you wore yesterday?",
+      },
+      {
+        id: 8,
+        question:
+          "Can you recall and describe in detail an interesting news article or TV show you watched this week?",
+      },
+      {
+        id: 9,
+        question: "Name the last three places you have visited prior to today.",
+      },
+      {
+        id: 10,
+        question: "What were the main ingredients in the last meal you cooked?",
+      },
+    ],
+  };
+
   const [recordedUrl, setRecordedUrl] = useState("");
   const [audioBlob, setAudioBlob] = useState(null);
   const mediaStream = useRef(null);
   const mediaRecorder = useRef(null);
   const chunks = useRef([]);
+  const [modelAnswer, setModelAnswer] = useState("");
   const startRecording = async () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({
@@ -39,16 +84,6 @@ export default function Upload() {
         console.log(blobFile);
         // saveAs(blobFile, "hackHarvardTest.wav");
         // fs.writeFileSync("audio.wav", Buffer.from(chunks));
-
-        axios({
-          method: "post",
-          url: "https://e0a8-67-134-206-60.ngrok-free.app/upload-audio",
-          data: formDataAudio,
-          headers: { "Content-Type": "multipart/form-data" },
-        }).then((resp) => {
-          console.log(resp);
-          console.log(resp.data.prediction);
-        });
       };
       mediaRecorder.current.start();
       console.log("Start recording.");
@@ -76,9 +111,12 @@ export default function Upload() {
 
     axios({
       method: "post",
-      url: "http://localhost:8000/upload-audio",
+      url: "https://e0a8-67-134-206-60.ngrok-free.app/upload-audio",
       data: formData,
       headers: { "Content-Type": "mutlipart/form-data" },
+    }).then((resp) => {
+      console.log(resp);
+      setModelAnswer(resp.data.prediction);
     });
   };
 
@@ -96,6 +134,24 @@ export default function Upload() {
       headers: { "Content-Type": "multipart/form-data" },
     });
   };
+
+  //   useEffect(() => {
+  //     console.log("hi!");
+  //     axios
+  //       .get("https://openai-aig.jacobcardoso2003.workers.dev")
+  //       .then((resp) => {
+  //         {
+  //           console.log(resp);
+  //         }
+  //       }, []);
+  //   });
+
+  let listOfQuestions = [];
+
+  for (let i = 0; i < cloudflareResp.questions.length; i++) {
+    console.log(i);
+    listOfQuestions.push(<li>{cloudflareResp.questions[i].question}</li>);
+  }
 
   return (
     <main>
@@ -118,74 +174,85 @@ export default function Upload() {
         </ul>
       </nav>
 
-      <section class="upload-form">
-        <h2>Record Your Voice for Analysis</h2>
-        <form
-          id="recordForm"
-          action="/submit-recording"
-          method="post"
-          enctype="multipart/form-data"
-        >
-          <label for="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Enter your name"
-            required
-          />
-
-          <label for="age">Age:</label>
-          <input
-            type="number"
-            id="age"
-            name="age"
-            placeholder="Enter your age"
-            required
-          />
-
-          <label for="gender">Gender:</label>
-          <select id="gender" name="gender" required>
-            <option value="">Select gender</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </select>
-
-          <label for="recording">Record Your Voice:</label>
-
-          <button
-            type="button"
-            id="recordButton"
-            class="record-btn"
-            onClick={startRecording}
+      <div className="flex flex-row">
+        <section class="upload-form">
+          <h2>Record Your Voice for Analysis</h2>
+          <form
+            id="recordForm"
+            action="/submit-recording"
+            method="post"
+            enctype="multipart/form-data"
           >
-            Start Recording
-          </button>
-          <button
-            type="button"
-            id="stopButton"
-            class="record-btn"
-            onClick={stopRecording}
-          >
-            Stop Recording
-          </button>
+            <label for="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Enter your name"
+              required
+            />
 
-          <div class="listening-animation" id="listeningAnimation">
-            <div class="pulse-ring"></div>
-            <div class="pulse-center"></div>
-          </div>
+            <label for="age">Age:</label>
+            <input
+              type="number"
+              id="age"
+              name="age"
+              placeholder="Enter your age"
+              required
+            />
 
-          <audio controls src={recordedUrl} />
-          <button type="submit" onClick={submitForAnalysis}>
-            Submit for Analysis
-          </button>
-        </form>
+            <label for="gender">Gender:</label>
+            <select id="gender" name="gender" required>
+              <option value="">Select gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
 
-        <a href="home.html" class="cta-button">
-          Back to Home
-        </a>
-      </section>
+            <label for="recording">Record Your Voice:</label>
+
+            <button
+              type="button"
+              id="recordButton"
+              class="record-btn"
+              onClick={startRecording}
+            >
+              Start Recording
+            </button>
+            <button
+              type="button"
+              id="stopButton"
+              class="record-btn"
+              onClick={stopRecording}
+            >
+              Stop Recording
+            </button>
+
+            <div class="listening-animation" id="listeningAnimation">
+              <div class="pulse-ring"></div>
+              <div class="pulse-center"></div>
+            </div>
+
+            <audio controls src={recordedUrl} />
+            <button type="submit" onClick={submitForAnalysis}>
+              Submit for Analysis
+            </button>
+          </form>
+
+          <a href="home.html" class="cta-button">
+            Back to Home
+          </a>
+        </section>
+
+        <section class="upload-form">
+          <h2>Dashboard</h2>
+
+          {listOfQuestions}
+
+          <h2>Results</h2>
+          {modelAnswer}
+        </section>
+      </div>
 
       <footer>
         <p>&copy; 2024 Dementia Detection Platform. All rights reserved.</p>
